90f183e92000d0a0e34f4618468bcf114b080bdc
kazu-taka <k.takahashi@nowall.co.jp>
2018-11-21 13:23:11 +0900

    Add B-2

--- customer.py
+++ customer.py
@@ -1,14 +1,26 @@
 class Customer:
-    def __init__(self, first_name, family_name):
+    def __init__(self, first_name, family_name, age):
         self.first_name = first_name
         self.family_name = family_name
+        self.age = age
 
     def full_name(self):
         return f"{self.first_name} {self.family_name}"
 
 
-ken = Customer(first_name="Ken", family_name="Tanaka")
-ken.full_name()
+# B-1
+# ken = Customer(first_name="Ken", family_name="Tanaka")
+# print(ken.full_name())
+#
+# tom = Customer(first_name="Tom", family_name="Ford")
+# print(tom.full_name())
 
-tom = Customer(first_name="Tom", family_name="Ford")
-tom.full_name()
+# B-2
+ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
+print(ken.age)  # 15 という値を返す
+
+tom = Customer(first_name="Tom", family_name="Ford", age=57)
+print(tom.age)  # 57 という値を返す
+
+ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
+print(ieyasu.age)  # 73 という値を返す
