e889ac22c7e82c53e9ce19a2ad8548cae85aa4ff
kazu-taka <k.takahashi@nowall.co.jp>
2018-11-21 13:30:49 +0900

    Add B-3

--- customer.py
+++ customer.py
@@ -7,6 +7,16 @@ class Customer:
     def full_name(self):
         return f"{self.first_name} {self.family_name}"
 
+    def entry_fee(self):
+        if self.age < 20:
+            return 1000
+
+        if 20 <= self.age < 65:
+            return 1500
+
+        if 65 <= self.age:
+            return 1200
+
 
 # B-1
 # ken = Customer(first_name="Ken", family_name="Tanaka")
@@ -16,11 +26,21 @@ class Customer:
 # print(tom.full_name())
 
 # B-2
+# ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
+# print(ken.age)  # 15 という値を返す
+#
+# tom = Customer(first_name="Tom", family_name="Ford", age=57)
+# print(tom.age)  # 57 という値を返す
+#
+# ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
+# print(ieyasu.age)  # 73 という値を返す
+
+# B-3
 ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
-print(ken.age)  # 15 という値を返す
+print(ken.entry_fee())  # 1000 という値を返す
 
 tom = Customer(first_name="Tom", family_name="Ford", age=57)
-print(tom.age)  # 57 という値を返す
+print(tom.entry_fee())  # 1500 という値を返す
 
 ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
-print(ieyasu.age)  # 73 という値を返す
+print(ieyasu.entry_fee())  # 1200 という値を返す
